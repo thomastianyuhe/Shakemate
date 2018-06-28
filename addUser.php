abf8cc7eb7c16e83162a16bd2c299f0fca691916
Zsanett <zsanett1220@gmail.com>
2018-06-28 14:30:34 +0100

    Image can be uploaded to server, forms not combined yet

---
+++ addUser.php
@@ -0,0 +1,22 @@
+<?php
+include_once("connection.php");
+
+$fullname = $_GET["fullname"];
+$phone = $_GET["phone"];
+$mail = $_GET["mail"];
+$skype = $_GET["skype"];
+$bio = $_GET["bio"];
+$profile = $_GET["profile"];
+
+$query = "INSERT INTO users SET fullname = '$fullname', phone = '$phone', mail = '$mail', skype = '$skype', bio = '$bio', profile = '$profile'";
+$result = mysqli_query($db_select, $query);
+
+if (!$result) {
+    # Error in the query
+    echo "Error running query $query : " . mysqli_error($db_select);
+    exit;
+}
+echo "Success";
+
+mysqli_close($db_select);
+?>
